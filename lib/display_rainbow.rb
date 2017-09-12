67d20842d688cdeff663876239eb83bd547ce054
gad gadov <omfgninja.pro@yandex.ru>
2017-09-12 18:16:56 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
-  puts ("R: " + colors[0] + ", O: ", colors[1] + ", O: ", colors[2] + ", Y: ", colors[3] + ", G: ", colors[4] + ", B: ", colors[5] + ", I: ", colors[6] + ", V: ", colors[7])
+  puts ("R: " + colors(0) + ", O: ", colors(1) + ", O: ", colors(2) + ", Y: ", colors(3) + ", G: ", colors(4) + ", B: ", colors(5) + ", I: ", colors(6) + ", V: ", colors(7))
 end
